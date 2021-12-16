805a39d2a32bc04c26889e3babf2413e57d5e042
jeandson <jeandsontb@gmail.com>
2021-12-16 16:10:33 -0300

    Implements Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -5,7 +5,19 @@ import S from './styles';
 const Dashboard = () => {
   return (
     <S.Container >
-      
+      <S.BoxHeader>
+
+        <S.BoxComponent>
+          <S.BoxUserInfo>
+            <S.Photo source={{ uri: 'https://avatars.githubusercontent.com/u/53402919?v=4' }} />
+            <S.BoxUser>
+              <S.TextGreeting>Olá</S.TextGreeting>
+              <S.TextTitleName>Jeandson</S.TextTitleName>
+            </S.BoxUser>
+          </S.BoxUserInfo>
+        </S.BoxComponent>
+
+      </S.BoxHeader>
     </S.Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,10 +1,43 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 export default {
   Container: styled.View`
     flex: 1;
-    background-color: ${({theme}) => theme.colors.background}
+    background-color: ${({theme}) => theme.colors.background};
   `,
-  
-  
+  BoxHeader: styled.View`
+    width: 100%;
+    height: ${RFPercentage(42)}px;
+    background-color: ${({theme}) => theme.colors.primary};
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+  `,
+  BoxComponent: styled.View`
+    width: 100%;
+    padding: 0 24px;
+  `,
+  BoxUserInfo: styled.View`
+    flex-direction: row;
+    align-items: center;
+  `,
+  Photo: styled.Image`
+    width: ${RFValue(48)}px;
+    height: ${RFValue(48)}px;
+    border-radius: 10px;
+  `,
+  BoxUser: styled.View`
+    margin-left: 17px;
+  `,
+  TextGreeting: styled.Text`
+    color: ${({theme}) => theme.colors.shape};
+    font-size: ${RFValue(18)}px;
+    font-family: ${({theme}) => theme.fonts.regular};
+  `,
+  TextTitleName: styled.Text`
+    color: ${({theme}) => theme.colors.shape};
+    font-size: ${RFValue(18)}px;
+    font-family: ${({theme}) => theme.fonts.bold};
+  `
 }
